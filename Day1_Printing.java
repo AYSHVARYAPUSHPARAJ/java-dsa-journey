f90949320e74ada3300d853ff61480e4c663ea6f
AYSHVARYA PUSHPARAJ <152197831+AYSHVARYAPUSHPARAJ@users.noreply.github.com>
2026-01-31 12:21:06 +0530

    Day1_Printing.java

---
+++ Day1_Printing.java
@@ -0,0 +1,6 @@
+public class Day1_Printing {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Starting my Java + DSA journey.");
+    }
+}
